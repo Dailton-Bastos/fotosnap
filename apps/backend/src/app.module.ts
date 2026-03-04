d2f294186c0cc4257462b1eabc35968124db5cf1
Dailton Bastos <dailtonbastos@gmail.com>
2026-03-03 22:54:59 -0300

    feat: auth backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { AuthGuard, AuthModule } from '@thallesp/nestjs-better-auth';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { DATABASE_CONNECTION } from './database/database-connection';
+import { APP_GUARD } from '@nestjs/core/constants';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    DatabaseModule,
+    AuthModule.forRootAsync({
+      imports: [DatabaseModule],
+      useFactory: (database: NodePgDatabase) => ({
+        auth: betterAuth({
+          database: drizzleAdapter(database, {
+            provider: 'pg',
+          }),
+        }),
+      }),
+      inject: [DATABASE_CONNECTION],
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
